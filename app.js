3841822f5f03090c8d87d674d0a2ad7712f5c361
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-02-14 17:28:51 +0000

    string type and template literal

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    if (showResult) {
+        console.log("".concat(phrase, " ").concat(n1 + n2));
+    }
+    else {
+        return n1 + n2;
+    }
 }
 // const number1 = '5'; throws TS warning errors
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
+var printResult = true;
+var resultPhrase = 'Result is: ';
+var result = add(number1, number2, printResult, resultPhrase);
 console.log(result);
