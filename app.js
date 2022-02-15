619f2b76f1e56a549f91463495f32405f682257a
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-02-15 10:14:25 +0000

    literal types with union types

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 // Union types
-function combine(n1, n2) {
+function combine(n1, // << union types
+n2, resultConversion // << literal type
+) {
     var result;
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
-        result = n1 + n2;
+    if ((typeof n1 === 'number' && typeof n2 === 'number') ||
+        resultConversion === 'as-number') {
+        result = +n1 + +n2;
     }
     else {
         result = n1.toString() + n2.toString();
     }
     return result;
 }
-var combineNumbers = combine(55, 7);
-var combineNames = combine('Ellie', 'May');
+var combineNumbers = combine(55, 7, 'as-number');
+var combineNames = combine('Ellie', 'May', 'as-text');
 console.log(combineNumbers, combineNames);
--- app.ts
+++ app.ts
@@ -1,16 +1,25 @@
 // Union types
 
-function combine(n1: number | string, n2: number | string) {
+function combine(
+  n1: number | string, // << union types
+  n2: number | string,
+  resultConversion: 'as-number' | 'as-text' // << literal type
+
+  // union types and literal types often used together
+) {
   let result;
-  if (typeof n1 === 'number' && typeof n2 === 'number') {
-    result = n1 + n2;
+  if (
+    (typeof n1 === 'number' && typeof n2 === 'number') ||
+    resultConversion === 'as-number'
+  ) {
+    result = +n1 + +n2;
   } else {
     result = n1.toString() + n2.toString();
   }
   return result;
 }
 
-const combineNumbers = combine(55, 7);
-const combineNames = combine('Ellie', 'May');
+const combineNumbers = combine(55, 7, 'as-number');
+const combineNames = combine('Ellie', 'May', 'as-text');
 
 console.log(combineNumbers, combineNames);
